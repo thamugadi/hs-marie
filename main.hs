e9657308a4c3c23291ea037a780d02f56f841c03
thamugadi <mdnh@protonmail.com>
2022-01-22 19:03:44 +0100

    Update main.hs

--- main.hs
+++ main.hs
@@ -2,7 +2,12 @@ import Data.List as DL
 import Data.Bits as DB
 import Data.Word as DW
 import Data.Char as DC
+
 data Instruction = Load Word16 | Store Word16 | Add Word16 | Sub Word16 | Input | Output | Halt | Skipcond Word16 | Jump Word16 deriving (Eq, Show, Read)
+
+data CPU = CPU Word16 Word16 Word16 Word16 Word16
+
+data Memory = Memory [Word16]
 
 decode :: Word16 -> Maybe Instruction
 decode instr =
@@ -35,51 +40,91 @@ encode instr =
           Skipcond sk -> 0x8000 + sk
           Jump  addr  -> 0x9000 + addr
 
-valid :: Maybe Instruction -> Instruction
-valid (Just a) = a
+accessMemory :: Memory -> Word16 -> Word16
+accessMemory (Memory mem) addr = if (w16i addr < length mem) then (mem!!(w16i addr))
+                                                        else fromIntegral 0 :: Word16
 
-marie :: [Word16] -> Word16 -> Word16 -> Word16 -> IO()
-marie memory ir ac pc =
-        case (decode ir) of
-          Just (Load x) -> marie memory (memory!!addrpc) (memory!!addrx) (pc+1) where
-                  addrx  = w16i x
-                  addrpc = w16i pc
-          Just (Store x) -> marie newmemory (memory!!addrpc) ac (pc+1) where
-                  addrx  = w16i x
-                  addrpc = w16i pc
-                  newmemory = take (addrx-1) memory ++ [ac] ++ drop (addrx+1) memory
-          Just (Add x) -> marie memory (memory!!addrpc) (ac+memory!!addrx) (pc+1) where
-                  addrx = w16i x
-                  addrpc = w16i pc
-          Just (Sub x) -> marie memory (memory!!addrpc) (ac-memory!!addrx) (pc+1) where
-                  addrx = w16i x
-                  addrpc = w16i pc
-          Just Input -> do
-                  hee <- getChar
-                  let byte = fromIntegral . ord $ hee :: Word16
-                  marie memory (memory!!addrpc) byte (pc+1) where 
-                          addrpc = w16i pc
-          Just Output -> do
-                  print ac
-                  marie memory (memory!!addrpc) ac (pc+1) where
-                          addrpc = w16i pc
-          Just Halt -> do
-                  putStrLn "Halting."
-                  pure ()
-          Just (Skipcond 0) -> if (ac < 0) then marie memory (memory!!(w16i pc + 1)) ac (pc+2)
-                             else marie memory (memory!!(w16i pc)) ac (pc+1)
-          Just (Skipcond 1) -> if (ac == 0) then marie memory (memory!!(w16i pc + 1)) ac (pc+2)
-                             else marie memory (memory!!(w16i pc)) ac (pc+1)
-          Just (Skipcond 2) -> if (ac > 0) then marie memory (memory!!(w16i pc + 1)) ac (pc+2)
-                             else marie memory (memory!!(w16i pc)) ac (pc+1)
-          Just (Skipcond 3) -> putStrLn "An error has occured."
-          Just (Jump x) -> marie memory (memory!!addrpc) ac (x+1) where
-                  addrpc = w16i x
-          Nothing -> putStrLn "!!BAD INSTRUCTION!!"
+modifyMemory :: Memory -> Word16 -> Word16 -> Memory
+modifyMemory (Memory mem) addr new =
+        if (w16i addr < length mem) then (Memory ((take ((w16i addr)-1) mem) ++ [new] ++ (drop ((w16i addr)) mem)) )
+                             else (Memory mem)
+
 
 startMarie :: Int -> [Word16] -> IO()
-startMarie memsize romlist = marie mem (mem!!0) 0 1 where
-        mem = romlist ++ (take memsize (repeat 0))
+startMarie memsize romlist = executeCycle firstInstr initialCPU initialmem where
+        initialmem = Memory (romlist ++ (take memsize (repeat 0)))
+        emptyCPU = CPU 0 0 0 0 0
+        initialCPU = fetchCycle emptyCPU initialmem
+        firstInstr = decodeCycle initialCPU
+
+fetchCycle :: CPU -> Memory -> CPU
+fetchCycle (CPU mar mbr ir ac pc) mem = CPU pc (accessMemory mem pc) (accessMemory mem pc) ac (pc+1)
+
+decodeCycle :: CPU -> Maybe Instruction
+decodeCycle (CPU mar mbr ir ac pc) = decode ir
+
+executeCycle :: Maybe Instruction -> CPU -> Memory -> IO()
+executeCycle Nothing _ _ = putStrLn "!!BAD INSTRUCTION!!"
+executeCycle (Just instr) cpu memory = do
+        case instr of
+                Load x -> newCycleNewCPU load x cpu memory
+                Store x -> newCycleNewMemory store x cpu memory 
+                Add x -> newCycleNewCPU add x cpu memory
+                Sub x -> newCycleNewCPU sub x cpu memory
+                Input -> newCycleNewIOCPU input cpu memory
+                Output -> newCycleAfterOutput output cpu memory
+                Halt -> putStrLn "Halting."
+                Jump x -> newCycleNewCPU jump x cpu memory
+                Skipcond 3 -> putStrLn "!!BAD INSTRUCTION!!"
+                Skipcond x -> newCycleNewCPU skipcond x cpu memory
+newCycleNewCPU :: (Word16 -> CPU -> Memory -> CPU) -> Word16 -> CPU -> Memory -> IO()
+newCycleNewCPU instr x cpu memory =
+        executeCycle (decodeCycle newCPU) (fetchCycle newCPU memory) memory where
+                newCPU = instr x cpu memory
+
+newCycleNewMemory :: (Word16 -> CPU -> Memory -> Memory) -> Word16 -> CPU -> Memory -> IO()
+newCycleNewMemory instr x cpu memory =
+        executeCycle (decodeCycle cpu) (fetchCycle cpu newMemory) newMemory where
+                newMemory = instr x cpu memory
+
+newCycleNewIOCPU :: (CPU -> IO CPU) -> CPU -> Memory -> IO()
+newCycleNewIOCPU instr cpu memory = do
+        newCPU <- instr cpu
+        executeCycle (decodeCycle newCPU) (fetchCycle newCPU memory) memory
+
+newCycleAfterOutput :: (CPU -> IO()) -> CPU -> Memory -> IO()
+newCycleAfterOutput instr cpu memory = do
+        instr cpu
+        executeCycle (decodeCycle cpu) (fetchCycle cpu memory) memory
+
+load :: Word16 -> CPU -> Memory -> CPU
+load x (CPU mar mbr ir ac pc) memory = CPU mar mbr ir (accessMemory memory x) pc
+store :: Word16 -> CPU -> Memory -> Memory
+store x (CPU mar mbr ir ac pc) memory = modifyMemory memory x ac
+add :: Word16 -> CPU -> Memory -> CPU
+add x (CPU mar mbr ir ac pc) memory = CPU mar mbr ir (ac+(accessMemory memory x)) pc
+sub :: Word16 -> CPU -> Memory -> CPU
+sub x (CPU mar mbr ir ac pc) memory = CPU mar mbr ir (ac-(accessMemory memory x)) pc  
+input :: CPU -> IO CPU
+input (CPU mar mbr ir ac pc) = do
+        ch <- getChar
+        let byte = fromIntegral . ord $ ch :: Word16
+        return (CPU mar mbr ir byte pc)
+output :: CPU -> IO()
+output (CPU mar mbr ir ac pc) = print ac
+jump :: Word16 -> CPU -> Memory -> CPU
+jump x (CPU mar mbr ir ac pc) memory = CPU mar mbr ir ac x
+
+nop :: CPU -> Memory -> CPU
+nop cpu memory = cpu
+
+skipcond :: Word16 -> CPU -> Memory -> CPU
+skipcond 0 (CPU mar mbr ir ac pc) memory = 
+        if (ac < 0) then (CPU mar mbr ir ac (pc+1)) else nop (CPU mar mbr ir ac pc) memory
+skipcond 1 (CPU mar mbr ir ac pc) memory =
+        if (ac == 0) then (CPU mar mbr ir ac (pc+1)) else nop (CPU mar mbr ir ac pc) memory
+skipcond 2 (CPU mar mbr ir ac pc) memory =
+        if (ac > 0) then (CPU mar mbr ir ac (pc+1)) else nop (CPU mar mbr ir ac pc) memory
 
 andb a b = (.&.) a b
 orb  a b = (.|.) a b
