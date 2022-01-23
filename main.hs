54ba5a96bf12c9ce28be789c5535aa82993ffdfa
thamugadi <mdnh@protonmail.com>
2022-01-23 21:09:13 +0100

    Update main.hs

--- main.hs
+++ main.hs
@@ -2,8 +2,10 @@ import Data.List
 import Data.Bits
 import Data.Word
 import Data.Char
+import Data.Maybe
 import Text.Read
 import Text.Printf
+
 data Instruction = Load Word16 | Store Word16 | Add Word16 | Sub Word16 | Input | Output | Halt | Skipcond Word16 | Jump Word16 deriving (Eq, Show, Read)
 data CPU = CPU Word16 Word16 Word16
 data Memory = Memory [Word16]
@@ -11,15 +13,15 @@ data Memory = Memory [Word16]
 decode :: Word16 -> Maybe Instruction
 decode instr =
         case op of
-          1 -> Just (Load addr)
-          2 -> Just (Store addr)
-          3 -> Just (Add addr)
-          4 -> Just (Sub addr)
+          1 -> Just $ Load addr
+          2 -> Just $ Store addr
+          3 -> Just $ Add addr
+          4 -> Just $ Sub addr
           5 -> Just Input
           6 -> Just Output
           7 -> Just Halt
-          8 -> Just (Skipcond sk)
-          9 -> Just (Jump addr)
+          8 -> Just $ Skipcond sk
+          9 -> Just $ Jump addr
           otherwise -> Nothing
         where 
                 op   = shiftR (andb instr 0xf000) 12
@@ -29,35 +31,37 @@ decode instr =
 encode :: Maybe Instruction -> Word16
 encode instr =
         case instr of
-          Just (Load  addr)  ->  0x1000 + addr
-          Just (Store addr)  ->  0x2000 + addr
-          Just (Add   addr)  ->  0x3000 + addr
-          Just (Sub   addr)  ->  0x4000 + addr
-          Just  Input        ->  0x5000
-          Just  Output       ->  0x6000
-          Just  Halt         ->  0x7000
-          Just (Skipcond sk) ->  0x8000 + (mod sk 3)
-          Just (Jump  addr)  ->  0x9000 + addr
-          Nothing ->             0xf000
+          Just (Load addr)  -> 0x1000 + addr
+          Just (Store addr) -> 0x2000 + addr
+          Just (Add addr)   -> 0x3000 + addr
+          Just (Sub addr)   -> 0x4000 + addr
+          Just Input        -> 0x5000
+          Just Output       -> 0x6000
+          Just Halt         -> 0x7000
+          Just (Skipcond sk)-> 0x8000 + (mod sk 3)
+          Just (Jump addr)  -> 0x9000 + addr
+          Nothing ->           0xffff
+
 accessMemory :: Memory -> Word16 -> Word16
-accessMemory (Memory mem) addr = if w16i addr < length mem then mem!!w16i addr
-                                                        else fromIntegral 0 :: Word16
+accessMemory (Memory mem) addr = maybe 0xffff id $ listToMaybe $ drop pos mem where
+        pos = w16i addr
 
 modifyMemory :: Memory -> Word16 -> Word16 -> Memory
-modifyMemory (Memory mem) addr new =
-        if w16i addr < length mem then Memory $ take ((w16i addr)-1) mem ++ [new] ++ drop (w16i addr) mem
-                             else Memory mem
-
+modifyMemory (Memory mem) addr new
+        | w16i addr < length mem && w16i addr >= 0 = Memory $ take (pos-1) mem ++ [new] ++ drop pos mem
+        | otherwise = Memory mem where
+                pos = w16i addr
 
 startMarie :: Int -> [Word16] -> IO()
 startMarie memsize romlist = executeCycle firstInstr initialCPU initialmem where
-        initialmem = Memory $ romlist ++ (take memsize (repeat 0))
+        annexmem   = take memsize $ repeat 0
+        initialmem = Memory $ romlist ++ annexmem
         emptyCPU   = CPU 0 0 0
         initialCPU = fetchCycle emptyCPU initialmem
         firstInstr = decodeCycle initialCPU
 
 fetchCycle :: CPU -> Memory -> CPU
-fetchCycle (CPU ir ac pc) mem = CPU (accessMemory mem pc) ac (pc+1)
+fetchCycle (CPU ir ac pc) mem = CPU (accessMemory mem pc) ac (pc+1) 
 
 decodeCycle :: CPU -> Maybe Instruction
 decodeCycle (CPU ir ac pc) = decode ir
@@ -79,7 +83,8 @@ executeCycle (Just instr) cpu memory = do
 newCycleNewCPU :: (Word16 -> CPU -> Memory -> CPU) -> Word16 -> CPU -> Memory -> IO()
 newCycleNewCPU instr x cpu memory =
         executeCycle nextInstruction newCPU memory where
-                newCPU = fetchCycle (instr x cpu memory) memory
+                cpuInstr = instr x cpu memory
+                newCPU = fetchCycle cpuInstr memory
                 nextInstruction = decodeCycle newCPU 
 newCycleNewMemory :: (Word16 -> CPU -> Memory -> Memory) -> Word16 -> CPU -> Memory -> IO()
 newCycleNewMemory instr x cpu memory =
@@ -123,11 +128,11 @@ nop cpu memory = cpu
 
 skipcond :: Word16 -> CPU -> Memory -> CPU
 skipcond 0 (CPU ir ac pc) memory = 
-        if (ac < 0) then CPU ir ac (pc+1) else nop (CPU ir ac pc) memory
+        if (ac < 0) then CPU ir ac (pc+1) else CPU ir ac pc
 skipcond 1 (CPU ir ac pc) memory =
-        if (ac == 0) then CPU ir ac (pc+1) else nop (CPU ir ac pc) memory
+        if (ac == 0) then CPU ir ac (pc+1) else CPU ir ac pc
 skipcond 2 (CPU ir ac pc) memory =
-        if (ac > 0) then CPU ir ac (pc+1) else nop (CPU ir ac pc) memory
+        if (ac > 0) then CPU ir ac (pc+1) else CPU ir ac pc
 
 andb a b = (.&.) a b
 orb  a b = (.|.) a b
